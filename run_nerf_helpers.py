ab2d3c5ab5103fb08eef8afdc05e1714fbb9c556
davidpengiupui <90153025+davidpengiupui@users.noreply.github.com>
2024-04-17 12:37:36 -0400

    Update run_nerf_helpers.py

--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -65,7 +65,7 @@ def get_embedder(multires, i=0):
 
 # Model
 class NeRF(nn.Module):
-    def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3, output_ch=4, skips=[4], use_viewdirs=False):
+    def __init__(self, D=8, W=256, input_ch=63, input_ch_views=27, output_ch=5, skips=[4], use_viewdirs=True):
         """ 
         """
         super(NeRF, self).__init__()
