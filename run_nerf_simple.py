e67322d9b8b99bcad8be5d7b86d744fbec931505
davidpengiupui <90153025+davidpengiupui@users.noreply.github.com>
2024-04-17 12:18:36 -0400

    Update run_nerf_simple.py

--- run_nerf_simple.py
+++ run_nerf_simple.py
@@ -179,9 +179,8 @@ def create_nerf(args):
 
     input_ch_views = 0
     embeddirs_fn = None
-    if args.use_viewdirs:
-        embeddirs_fn, input_ch_views = get_embedder(args.multires_views, args.i_embed)
-    output_ch = 5 if args.N_importance > 0 else 4
+    embeddirs_fn, input_ch_views = get_embedder(args.multires_views, args.i_embed)
+    output_ch = 5 
     skips = [4]
     model = NeRF(D=args.netdepth, W=args.netwidth,
                  input_ch=input_ch, output_ch=output_ch, skips=skips,
