94caed3fd1364b6feebcf3378f738df5fa3cd4d2
davidpengiupui <90153025+davidpengiupui@users.noreply.github.com>
2024-04-17 11:56:47 -0400

    Update run_nerf_simple.py

--- run_nerf_simple.py
+++ run_nerf_simple.py
@@ -534,33 +534,25 @@ def train():
     args = parser.parse_args()
 
     # Load data
-    K = None
-    
     images, poses, render_poses, hwf, i_split = load_blender_data(args.datadir, args.half_res, args.testskip)
     print('Loaded blender', images.shape, render_poses.shape, hwf, args.datadir)
     i_train, i_val, i_test = i_split
 
     near = 2.
     far = 6.
-
-    if args.white_bkgd:
-        images = images[...,:3]*images[...,-1:] + (1.-images[...,-1:])
-    else:
-        images = images[...,:3]
-
+ 
+    images = images[...,:3]*images[...,-1:] + (1.-images[...,-1:])
     
-
     # Cast intrinsics to right types
     H, W, focal = hwf
     H, W = int(H), int(W)
     hwf = [H, W, focal]
 
-    if K is None:
-        K = np.array([
-            [focal, 0, 0.5*W],
-            [0, focal, 0.5*H],
-            [0, 0, 1]
-        ])
+    K = np.array([
+        [focal, 0, 0.5*W],
+        [0, focal, 0.5*H],
+        [0, 0, 1]
+    ])
 
     if args.render_test:
         render_poses = np.array(poses[i_test])
