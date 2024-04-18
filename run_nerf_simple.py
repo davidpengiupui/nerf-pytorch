ba1f679550a0fa13c67ad3b1b9b3dba711139128
davidpengiupui <90153025+davidpengiupui@users.noreply.github.com>
2024-04-18 15:26:13 -0400

    Update run_nerf_simple.py

--- run_nerf_simple.py
+++ run_nerf_simple.py
@@ -273,11 +273,11 @@ def raw2outputs(raw, z_vals, rays_d, raw_noise_std=0, white_bkgd=False, pytest=F
             np.random.seed(0)
             noise = np.random.rand(*list(raw[...,3].shape)) * raw_noise_std
             noise = torch.Tensor(noise)
-
+    # Line 276 - 280 for Equation 3
     alpha = raw2alpha(raw[...,3] + noise, dists)  # [N_rays, N_samples]
     # weights = alpha * tf.math.cumprod(1.-alpha + 1e-10, -1, exclusive=True)
     weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1)), 1.-alpha + 1e-10], -1), -1)[:, :-1]
-    rgb_map = torch.sum(weights[...,None] * rgb, -2)  # [N_rays, 3]
+    rgb_map = torch.sum(weights[...,None] * rgb, -2)  # [N_rays, 3] 
 
     depth_map = torch.sum(weights * z_vals, -1)
     disp_map = 1./torch.max(1e-10 * torch.ones_like(depth_map), depth_map / torch.sum(weights, -1))
